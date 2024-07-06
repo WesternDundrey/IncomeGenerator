fbb4c1eb09ccc31bbd2578cd130bde146bd5d0c4
westerndundrey <hijackedpuffin@gmail.com>
2024-07-06 20:07:55 +0800

    finished project, animations added also

--- income/src/main.rs
+++ income/src/main.rs
@@ -1,24 +1,36 @@
 slint::include_modules!();
 
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
 
-const TAXPER: f64 = .30;
-const OWNERPER: f64 = .55;
-const PROFITPER: f64 = .05;
-const OPEXPER: f64 = .10;
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+
     let ui_handle = ui.as_weak();
-    ui.on_divide_income(move |string {
+    ui.on_divide_income(move |string| {
         let ui = ui_handle.unwrap();
         let num: f64 = string.trim().parse().unwrap();
-        let tax: f64 = num *TAXPER;
-        let owner: f64 = num *OWNERPER;
-        let profit: f64 = num *PROFITPER;
-        let opex: f64 = num *OPEXPER;
-        let result=fortmat!("Taxes:{:2}\nOwner:{:2}\nProfit:{:2}\nOpEx:{:2}", {tax}, {owner}, {profit}, {opex})
-        ui.set_results(result);
+        let taxes: f64 = num * TAXPER;
+        let owner: f64 = num * OWNERPER;
+        let profit: f64 = num * PROFITPER;
+        let opex: f64 = num * OPEXPER;
+        let result = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}", taxes, owner, profit, opex);
+        ui.set_results(result.into());
     });
 
     ui.run()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn equals_100() {
+        let result = TAXPER + OWNERPER + PROFITPER + OPEXPER;
+        let formatted = f64::trunc(result  * 100.0) / 100.0;
+        assert_eq!(formatted, 1.00);
+    }
+}
