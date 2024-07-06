32129c3be811e51a510030e96e9859ad0b32cdf8
westerndundrey <hijackedpuffin@gmail.com>
2024-07-06 20:05:14 +0800

    nice

--- income/src/main.rs
+++ income/src/main.rs
@@ -8,9 +8,15 @@ const OPEXPER: f64 = .10;
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
     let ui_handle = ui.as_weak();
-    ui.on_divid_incoe(move || {
+    ui.on_divide_income(move |string {
         let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+        let num: f64 = string.trim().parse().unwrap();
+        let tax: f64 = num *TAXPER;
+        let owner: f64 = num *OWNERPER;
+        let profit: f64 = num *PROFITPER;
+        let opex: f64 = num *OPEXPER;
+        let result=fortmat!("Taxes:{:2}\nOwner:{:2}\nProfit:{:2}\nOpEx:{:2}", {tax}, {owner}, {profit}, {opex})
+        ui.set_results(result);
     });
 
     ui.run()
